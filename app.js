8c03845eef308bf0f10e421f137886ae200ffbef
Thiago <thiiagoo106@gmail.com>
2025-01-17 15:56:27 -0300

    Criação projeto

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let listaAmigos = document.querySelector("#listaAmigos");
+let sorteado = []
+
+
+
+function adicionarAmigo(){
+
+      const nome = document.querySelector("#amigo");
+      if(nome.value === ""){
+            alert("Por favor, insira um nome.");
+      }else{
+            amigos.push(nome.value);
+            limparCampo(nome)
+            listarAmigos()
+      }
+}
+
+function listarAmigos(){
+      listaAmigos.innerHTML = "";
+      amigos.forEach(amigo => {
+            const li = document.createElement("li");
+            li.textContent = amigo;
+            listaAmigos.appendChild(li);
+          });
+}
+
+function limparCampo(campo){
+      campo.value = null;
+}
+
+function sortearAmigo(){
+      let resultado = document.querySelector("#resultado");
+
+
+      if (sorteado.length === amigos.length) {
+            resultado.innerHTML = "Todos os amigos já foram sorteados!";
+            return;
+      }
+
+      let nomeSorteado;
+
+      do{
+            nomeSorteado = Math.floor(Math.random() * amigos.length)
+            resultado.innerHTML = amigos[nomeSorteado];
+      }while(sorteado.includes(nomeSorteado));
+
+      sorteado.push(nomeSorteado)
+
+
+}
